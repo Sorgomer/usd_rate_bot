17b11394bd99857fa4b398cfc45265a55a53283d
sorgomer <sorgomer@yandex.ru>
2025-12-07 09:37:18 +0530

    a

--- bot/states.py
+++ bot/states.py
@@ -2,6 +2,6 @@ from aiogram.fsm.state import StatesGroup, State
 
 
 class SettingsStates(StatesGroup):
-    waiting_timezone = State()
+    waiting_city = State()
     waiting_currency = State()
     waiting_time = State()
